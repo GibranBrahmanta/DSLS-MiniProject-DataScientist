379ea36aabac0722f9a500c719ee982eee7598ef
GibranBrahmanta <gibranbrahmanta@gmail.com>
2023-02-07 12:28:57 +0700

    Modify pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -10,16 +10,19 @@ logger.setLevel(logging.INFO)
 
 from data.make_dataset import DatasetGenerator
 from features.build_features import FeatureEngineer
+from models.train_evaluate_time_series import TimeSeriesExperiment
 
 class Pipeline:
 
     def __init__(self) -> None:
         self.dataset_generator = DatasetGenerator()
         self.feature_engineer = FeatureEngineer()
+        self.time_series_experiment = TimeSeriesExperiment()
 
     def run(self):
         self.dataset_generator.create_dataset()
         self.feature_engineer.build_feature()
+        self.time_series_experiment.pipeline()
         
 
 
